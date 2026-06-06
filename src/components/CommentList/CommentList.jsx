f2070a7d42a1a8117d52817555051b168e13d386
Mykhailo Oliinyk <mykhailo.oliinyk9@gmail.com>
2026-06-06 20:42:44 +0300

    fixes

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -3,7 +3,7 @@ import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => {
   return (
-    <div className="PostList">
+    <div className="CommentList">
       {comments.map(comment => {
         return <CommentInfo key={comment.id} comment={comment} />;
       })}
